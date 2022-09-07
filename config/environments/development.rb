81fd1072522471b10576369a5767e4f0e98f4c74
Lhoussaine bouaik <bouaik.lhou@gmail.com>
2022-09-07 19:08:26 +0100

    add hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.hosts << "chat-tailwind-production.up.railway.app"
   
 
   # In the development environment your application's code is reloaded any time
